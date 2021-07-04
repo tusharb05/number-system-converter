e46c9a0ae471619c3919967abf4d955f2a0ac037
Ad Astra!!! <70359572+tusharb05@users.noreply.github.com>
2021-07-04 15:50:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './components/Home';
+// import Home from './components/Home';
 import Converter from './components/Converter'
 
 function App() {
